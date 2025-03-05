c7dd6927c2614a8cf19ed7ed80e4fe1680eab424
namai keijiro <namaikeijiro@gmail.com>
2025-03-06 04:12:31 +0900

    aaa

---
+++ server/api/plants.put.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+    const id = body.id;
+    const name = body.name;
+
+    if (!(id && name)) return createError({ statusCode: 400, statusMessage: "Missing id and name"});
+
+    let plant = null;
+
+    if (id && name) {
+        plant = await prisma.plants.update({
+            where: {
+                id: id,
+            },
+            data: {
+                name: name,
+            },
+        });
+    }
+
+    return plant;
+});
