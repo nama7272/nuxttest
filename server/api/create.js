384a1d2ffda6bb91918b0dbc98d92f63b6ede529
namai keijiro <namaikeijiro@gmail.com>
2025-03-07 02:59:30 +0900

    aaa

---
+++ server/api/create.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: body.name,
+                email: body.email,
+            },
+        })
+        return user
+    } catch (error) {
+        throw createError({ statusCode: 500, statusMessage: 'Internal Server Error' })
+    } finally {
+        await prisma.$disconnect()
+    }
+})
