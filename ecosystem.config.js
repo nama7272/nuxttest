cc8a0653562e24755c6fc2dd41a8dcf42746b002
namai keijiro <namaikeijiro@gmail.com>
2025-03-11 02:14:34 +0900

    dsadas

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,14 @@
-export default {
+module.exports = {
     apps: [
         {
             name: 'NuxtAppName',
             exec_mode: 'cluster',
             instances: 'max', // またはインスタンスの数
-            script: './node_modules/nuxt/bin/nuxt.js',
-            args: 'start'
+            script: './.output/server/index.mjs', // ビルドされたアプリケーションのパス
+            args: '', // 引数は不要
+            env: {
+                NODE_ENV: 'production' // 本番環境であることを明示的に設定
+            }
         }
     ]
-}
+};
