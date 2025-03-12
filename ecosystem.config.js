5bc8291cbc90a434ba287867b08f901fd38b8f28
namai keijiro <namaikeijiro@gmail.com>
2025-03-13 02:03:49 +0900

    adsad

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,5 @@ module.exports = {
         instances: 1,
         script: './.output/server/index.mjs',
         args: '',
-        env: {
-            NODE_ENV: 'production',
-            DATABASE_URL: "mysql://namai5_111:nama7272@localhost:3306/namai5_test"
-        }
     }]
 };
